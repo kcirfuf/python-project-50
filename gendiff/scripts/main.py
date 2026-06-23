c983cf69baf5aafc1692952d87ff61c09ca91202
Ilia Anufriev <ianufriev@marfa-tech.com>
2026-06-23 10:46:52 +0200

    step 2

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -6,7 +6,13 @@ def main():
                         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f',
+        '--format',
+        dest="format",
+        default="json",
+        help='set format of output')
     args = parser.parse_args()
-    
+
 if __name__ == "__main__":
     main()
